9ad2af1fcb67dc8c2c90c5e90dcb942c30cf4e06
jarymor <eu.ostap@yandex.ru>
2024-04-14 13:49:48 +0300

    .

---
+++ src/test/java/ru/den_ostap/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.den_ostap.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
