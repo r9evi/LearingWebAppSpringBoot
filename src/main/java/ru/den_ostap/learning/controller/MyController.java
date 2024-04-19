f9bbfccaee9cde1eefb68213b7c21931aa9830c7
Podzorov D.Y. <dp.podzorov@gmail.com>
2024-04-19 12:53:29 +0300

    feat: added some views and simple GET request to views from controller

--- src/main/java/ru/den_ostap/learning/controller/MyController.java
+++ src/main/java/ru/den_ostap/learning/controller/MyController.java
@@ -2,10 +2,35 @@ package ru.den_ostap.learning.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 
+    @GetMapping("/Learn-platform")
+    public String goToIndex() {
+        return "index";
+    }
+
+    @GetMapping("/ready-courses")
+    public String goToReadyCourses() {
+        return "ready_courses";
+    }
+
+    @GetMapping("/create-course")
+    public String goToCreateCourse() {
+        return "create_course";
+    }
+
+    @GetMapping("/resources")
+    public String goToResources() {
+        return "resources";
+    }
+
+    @GetMapping("/resources/topic")
+    public String goToTopic() {
+        return "ready_courses";
+    }
 
 
 }
